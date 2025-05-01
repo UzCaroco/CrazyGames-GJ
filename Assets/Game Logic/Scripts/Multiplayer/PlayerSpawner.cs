50e5ce03d51f5d6390ee2ba7139572e4e85cfe4c
Rosalie Alves <rosalieslv@gmail.com>
2025-05-01 17:47:38 -0300

    bora

--- Assets/Game Logic/Scripts/Multiplayer/PlayerSpawner.cs
+++ Assets/Game Logic/Scripts/Multiplayer/PlayerSpawner.cs
@@ -1,15 +1,17 @@
+using CrazyGames;
 using Fusion;
 using UnityEngine;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject playerPrefab;
+    [SerializeField] private NetworkRunner runner;
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity);
+            Runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, inputAuthority: runner.LocalPlayer);
         }
     }
 }
