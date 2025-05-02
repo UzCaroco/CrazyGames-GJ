7bd7aff8aea691c6b7bb1e3395f405f4ae4faf1e
Rosalie Alves <rosalieslv@gmail.com>
2025-05-01 23:08:42 -0300

    .

--- Assets/Game Logic/Scripts/Gameplay/Mission/MissionCollectCoin.cs
+++ Assets/Game Logic/Scripts/Gameplay/Mission/MissionCollectCoin.cs
@@ -6,20 +6,14 @@ using UnityEngine;
 public class MissionCollectCoin : Missions
 {
     [Header("Mission 1 - CC")]
-    byte any;
-    //[Networked} byte totalPLayers;
-    //[Networked] byte takeCoin;
-    [Networked] private Vector2 BombPosition { get; set; }
+    private List<PlayerController> playersActive = new List<PlayerController>();
+
+    sbyte takeCoin;
+
+    // x = -10.5 até 10.5 y = - 7 ate 7 
+    [Networked] private Vector2 coinPosition { get; set; }
     [Networked] private bool isInitialized { get; set; }
 
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        StartMission();
-    }
     public override void FixedUpdateNetwork()
     {
        // base.FixedUpdateNetwork();
@@ -27,18 +21,59 @@ public class MissionCollectCoin : Missions
 
     public override void CallStartMission()
     {
-        print("Begginng");
+        StartMission();
     }
     public override void CallCompleteMission()
     {
         CompleteMission();
     }
+
+    void GetTotalPlayers()
+    {
+        foreach (var playerController in playersActive)
+        {
+            if (playerController != null)
+            {
+                playersActive.Add(playerController);
+                takeCoin++;
+            }
+        }
+    }
+
+    void DrawPos()
+    {
+        coinPosition = new Vector2((float)Random.Range(-10.5f, 10.5f), (float)Random.Range(-7f, 7f));
+    }
+
+    void ControlSpwnCoins()
+    {
+        if (!isInitialized) 
+        { 
+            while(takeCoin > 0)
+            {
+                takeCoin--;
+                DrawPos();
+
+
+            }
+        }
+    }
+
+    void Instanciate()
+    {
+
+    }
+
     protected override void StartMission()
     {
         Debug.Log("Collect Coin, Beginning!");
     }
     protected override void CompleteMission()
     {
+
+        takeCoin = 0;
+        isInitialized = false;
+        
         Debug.Log("Collect Coin, Finish!");
     }
 }
