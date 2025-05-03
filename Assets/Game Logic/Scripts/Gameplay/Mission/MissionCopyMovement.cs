23b41ea1c72171ba02bc20f5fc4d3f8b0d0c6a01
Rosalie Alves <rosalieslv@gmail.com>
2025-05-02 22:28:25 -0300

    é noite

--- Assets/Game Logic/Scripts/Gameplay/Mission/MissionCopyMovement.cs
+++ Assets/Game Logic/Scripts/Gameplay/Mission/MissionCopyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using CrazyGames;
 using Fusion;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,8 @@ using static PlayerCopyMovementController;
 public class MissionCopyMovement : Missions
 {
     [Header("Mission 2 - CM")]
+    [SerializeField] private NetworkRunner runner;
+
     [SerializeField] private GameObject painelmovement;
 
     [SerializeField] private NetworkInputHandler networkInputHandler;
@@ -15,33 +18,78 @@ public class MissionCopyMovement : Missions
     [SerializeField] private Sprite[] spritesMovement = new Sprite[4];
 
     sbyte[] copyThisMovement = new sbyte[4];
-    sbyte[] playerMovementCopy = new sbyte[4];
+    sbyte totalImages = 0;
+    //sbyte[] playerMovementCopy = new sbyte[4];
 
     Dictionary<PlayerRef, sbyte[]> playerInputs = new();
     Dictionary<PlayerRef, int> playerCorrectCount = new();
 
+    [SerializeField] bool isMission = false;
 
     void Start()
     {
-        
+        StartMission();
     }
     void Update()
     {
-        //StartMission();
+        /*print("UPDATE"); 
+
+        if (isMission)
+        {
+            StartMission();
+            isMission = false;
+        }*/
     }
     public override void FixedUpdateNetwork()
     {
+        print("NETWORK");
+        if (isMission)
+        {
+            ResultMovement();
+        }
+    }
+
+    public override void CallStartMission()
+    {
         StartMission();
+        print("Begginng");
+    }
+    public override void CallCompleteMission()
+    {
+        CompleteMission();
     }
 
     void DrawSequence()
     {
         for (int i = 0; i < copyThisMovement.Length; i++)
         {
-            copyThisMovement[i] = (sbyte)Random.Range(0, 5);
-
-            imageMovement.sprite = spritesMovement[copyThisMovement[i]];
+            copyThisMovement[i] = (sbyte)Random.Range(0, 4);
         }
+
+        StartCoroutine(TimeForSeeMovement());
+    }
+
+    IEnumerator TimeForSeeMovement()
+    {
+        imageMovement.sprite = spritesMovement[copyThisMovement[totalImages]];
+        
+        yield return new WaitForSeconds(0.5f);
+
+        totalImages++;
+        
+        if (totalImages > 3)
+        {
+            painelmovement.SetActive(false);
+            StopCoroutine(TimeForSeeMovement());
+            isMission = true;
+        }
+
+        else
+        {
+            StartCoroutine(TimeForSeeMovement());
+        }
+
+        
     }
 
     void ResultMovement()
@@ -73,38 +121,23 @@ public class MissionCopyMovement : Missions
         }
     }
 
-    void WaitInput()
-    {
-        foreach (var player in PlayerCopyMovementController.AllPlayers)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                while (player.Movements.Get(i) == 0)
-                {
-
-                }
-            }
-        }
-    }
-
-    public override void CallStartMission()
-    {
-        print("Begginng");
-    }
-    public override void CallCompleteMission()
-    {
-        CompleteMission();
-    }
-
     protected override void StartMission()
     {
+        runner = FindObjectOfType<NetworkRunner>();
         painelmovement.SetActive(true);
         networkInputHandler = GetComponent<NetworkInputHandler>();
         networkInputHandler.enabled = true;
+
+        DrawSequence();
         Debug.Log("COPY THE MOVEMENT, Beginning!");
     }
     protected override void CompleteMission()
     {
+        for (int i = 0; i < 4; i++)
+        {
+            copyThisMovement[i] = 0;
+        }
+        isMission = false;
         painelmovement.SetActive(false);
         networkInputHandler.enabled = false;
         Debug.Log("COPY THE MOVEMENT, Finish!");
