053b8237393c4a48860ee14fb1492813dd5cfc7c
Luiz <164922548+UzCaroco@users.noreply.github.com>
2025-05-04 10:34:37 -0300

    ajustes move e bomb

--- Assets/Game Logic/Scripts/Gameplay/Mission/MissionMove.cs
+++ Assets/Game Logic/Scripts/Gameplay/Mission/MissionMove.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using CrazyGames;
 using Fusion;
 using UnityEngine;
 
 public class MissionMove : Missions
 {
+    NetworkRunner runner;
     [Header("Mission 4 - M")]
     byte any;
     void Start()
@@ -20,7 +23,31 @@ public class MissionMove : Missions
     }
     public override void CallStartMission()
     {
-        print("Begginng");
+        runner = FindObjectOfType<NetworkRunner>(); //Pega o NetworkRunner na cena
+        Debug.Log("Quantidade de players ativos: " + runner.ActivePlayers.Count());
+
+
+        foreach (var player in runner.ActivePlayers)
+        {
+            Debug.Log("Checando player: " + player);
+
+            var networkObject = runner.GetPlayerObject(player); //Percorre os objetos de rede ativos (Players)
+
+            Debug.Log("NETWORKOBJECT VAZIO??: " + networkObject);
+            if (networkObject != null) //Verifica se o objeto de rede não é nulo
+            {
+                Debug.Log("EXISTE O NETWORKOBJECT");
+
+                PlayerController playerController = networkObject.GetComponent<PlayerController>(); //Pega o script PlayerController do objeto de rede
+
+                if (playerController != null)
+                {
+
+                    playerController.move = true; // ativa o booleano
+
+                }
+            }
+        }
     }
     public override void CallCompleteMission()
     {
