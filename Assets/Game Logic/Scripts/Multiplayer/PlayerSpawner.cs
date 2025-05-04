8fbb25e71659367be2209948c233cb3bbbcfdb85
Rosalie Alves <rosalieslv@gmail.com>
2025-05-04 17:36:29 -0300

    ll

--- Assets/Game Logic/Scripts/Multiplayer/PlayerSpawner.cs
+++ Assets/Game Logic/Scripts/Multiplayer/PlayerSpawner.cs
@@ -27,8 +27,8 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 
         if (player == Runner.LocalPlayer) // ou Runner.IsServer se tiver usando Server/Client
         {
-            randomSkin.PlayerIsSpawned();
-            anim.runtimeAnimatorController = animatorOverrideController[randomSkin.SetNumRandom()];
+            //randomSkin.PlayerIsSpawned();
+            //anim.runtimeAnimatorController = animatorOverrideController[randomSkin.SetNumRandom()];
             /*print("TA O QUE" + gameManager);
 
 
