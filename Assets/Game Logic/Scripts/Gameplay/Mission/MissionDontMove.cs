dee385a51cc5abc6ce48957c6d283a43f9a07319
Luiz <164922548+UzCaroco@users.noreply.github.com>
2025-05-04 10:14:27 -0300

    ajustes dontmove copymove

--- Assets/Game Logic/Scripts/Gameplay/Mission/MissionDontMove.cs
+++ Assets/Game Logic/Scripts/Gameplay/Mission/MissionDontMove.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using CrazyGames;
 using Fusion;
 using UnityEngine;
 
@@ -7,6 +9,8 @@ public class MissionDontMove : Missions
 {
     [Header("Mission 3 - DM")]
     byte any;
+
+    NetworkRunner runner;
 
     void Start()
     {
@@ -22,6 +26,31 @@ public class MissionDontMove : Missions
     }
     public override void CallStartMission()
     {
+        runner = FindObjectOfType<NetworkRunner>(); //Pega o NetworkRunner na cena
+        Debug.Log("Quantidade de players ativos: " + runner.ActivePlayers.Count());
+
+
+        foreach (var player in runner.ActivePlayers)
+        {
+            Debug.Log("Checando player: " + player);
+
+            var networkObject = runner.GetPlayerObject(player); //Percorre os objetos de rede ativos (Players)
+
+            Debug.Log("NETWORKOBJECT VAZIO??: " + networkObject);
+            if (networkObject != null) //Verifica se o objeto de rede não é nulo
+            {
+                Debug.Log("EXISTE O NETWORKOBJECT");
+
+                PlayerController playerController = networkObject.GetComponent<PlayerController>(); //Pega o script PlayerController do objeto de rede
+
+                if (playerController != null)
+                {
+                    
+                    playerController.dontMove = true; // ativa o booleano
+
+                }
+            }
+        }
     }
     public override void CallCompleteMission()
     {
